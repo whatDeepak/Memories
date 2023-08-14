e773fee67ae3dd208524216c1b3826dd117b103c
Deepak <deekay9357@gmail.com>
2023-08-14 18:10:36 +0530

    Memories@1.0.1 DatePicker

--- app/src/main/java/com/vyarth/memories/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/vyarth/memories/AddHappyPlaceActivity.kt
@@ -1,10 +1,30 @@
 package com.vyarth.memories
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toolbar
+import androidx.appcompat.widget.AppCompatEditText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+
+
+    /**
+     * An variable to get an instance calendar using the default time zone and locale.
+     */
+    private var cal = Calendar.getInstance()
+
+    /**
+    * A variable for DatePickerDialog OnDateSetListener.
+    * The listener used to indicate the user has finished selecting a date. Which we will be initialize later on.
+    */
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -16,5 +36,46 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         toolbarAddPlace.setNavigationOnClickListener {
             onBackPressed()
         }
+
+
+        // https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/
+        // create an OnDateSetListener
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                updateDateInView()
+
+            }
+        val etDate: AppCompatEditText =findViewById<AppCompatEditText>(R.id.et_date)
+        etDate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener, // This is the variable which have created globally and initialized in setupUI method.
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR), // Here the cal instance is created globally and used everywhere in the class where it is required.
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+    }
+
+    /**
+     * A function to update the selected date in the UI with selected format.
+     * This function is created because every time we don't need to add format which we have added here to show it in the UI.
+     */
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // A date format
+        val etDate: AppCompatEditText =findViewById<AppCompatEditText>(R.id.et_date)
+        etDate.setText(sdf.format(cal.time).toString()) // A selected date using format which we have used is set to the UI.
     }
 }
